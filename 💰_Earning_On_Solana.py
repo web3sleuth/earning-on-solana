a17fd8c565fb6751db477e274720184517727acd
web3sleuth <149721671+web3sleuth@users.noreply.github.com>
2024-01-22 18:41:03 +0100

    Update 💰_Earning_On_Solana.py

--- 💰_Earning_On_Solana.py
+++ 💰_Earning_On_Solana.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from streamlit_extras.colored_header import colored_header
 
+st.cache_data.clear()
+
 st.set_page_config(
     page_title="Earning On Solana",
     page_icon=None,
